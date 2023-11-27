8b05bd9bfd82f0af934646c9c930f54debd9fd1c
ehdtn0321 <ehdtn0321@naver.com>
2023-11-27 20:50:26 +0900

    💄: css setting - colors - fonts - reset.css

---
+++ src/styles/colors.ts
@@ -0,0 +1,30 @@
+// grayscale
+export const grayScale = {
+  900: "#191A1A",
+  800: "#252627",
+  700: "#303133",
+  600: "#4E5053",
+  500: "#717479",
+  400: "#9DA1A8",
+  300: "#B4BBC2",
+  200: "#D3D5DB",
+  100: "#E5E7EC",
+  50: "#FBFBFB",
+};
+
+// primary color
+export const primary = {
+  600: "#83FFA0",
+  700: "#95FFAD",
+};
+
+export const secondary = {
+  greenLignt: "#87EF63",
+  greendark: "#4F6647",
+  pinkLight: "#F07BC1",
+  pinkDark: "#7B4F69",
+  blueLight: "#5FB3F0",
+  blueDark: "#536A7A",
+  orangeLight: "#F5A66E",
+  orangeDark: "#7D6350",
+};
