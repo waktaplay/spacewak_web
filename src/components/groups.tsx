bb0d020ac1f392182de6bcb9ca3c640d5be61d6d
ehdtn0321 <ehdtn0321@naver.com>
2023-12-16 01:34:07 +0900

    🔨 update: group image slide 기능 추가
    좌우 onClick 이동 이벤트 추가

--- src/components/groups.tsx
+++ src/components/groups.tsx
@@ -2,54 +2,79 @@ import Wakgood from '@/assets/profile/wakdoo512.png'
 import Isedol from '@/assets/profile/isegye_idol_group.png'
 import { css } from '@emotion/react'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { MutableRefObject, useEffect, useRef, useState } from 'react'
+
+const imageWidth = 440
+const gapWidth = 40
 
 export function Groups() {
-  const [start, setStart] = useState(1)
-  const [end, setEnd] = useState(3)
   const imageList = [
+    { src: Isedol, alt: '이세돌', ko: '이세계아이돌', en: 'ISEGYE IDOL' },
     { src: Wakgood, alt: '왁두', ko: '우왁굳', en: 'WAKGOOD' },
     { src: Isedol, alt: '이세돌', ko: '이세계아이돌', en: 'ISEGYE IDOL' },
     { src: Wakgood, alt: '왁두', ko: '우왁굳', en: 'WAKGOOD' },
-    // { src: Isedol, alt: '왁두', ko: '우왁굳', en: 'WAKGOOD' },
   ]
 
-  imageList.splice(end)
+  const ref = useRef<HTMLDivElement>(null)
+
+  const moveWidth = imageWidth + gapWidth
+
+  const [scroll, setScroll] = useState(0)
+  const childrenWidth =
+    ref.current?.childNodes.length * 440 + (ref.current?.childNodes.length - 1) * 40 ||
+    imageList.length * 440 + (imageList.length - 1) * 40
+  const maxScrollWidth = childrenWidth - imageWidth * 4 + gapWidth * 3
+
+  const goPrev = () => {
+    if (scroll <= 0) {
+      return
+    }
+    ref.current.scrollTo({ left: scroll - moveWidth, top: 0, behavior: 'smooth' })
+    const decreaseScroll = scroll - moveWidth < 0 ? 0 : scroll - moveWidth
+    setScroll(decreaseScroll)
+  }
+
+  const goNext = () => {
+    if (scroll > maxScrollWidth) {
+      return
+    }
+    ref.current.scrollTo({ left: scroll + moveWidth, top: 0, behavior: 'smooth' })
+    const increaseScroll = scroll + moveWidth > maxScrollWidth ? maxScrollWidth : scroll + moveWidth
+    setScroll(increaseScroll)
+  }
 
   return (
     <div css={groupWrapCss}>
       <p css={css({ fontSize: '32px', fontWeight: 700, color: 'white', display: 'flex', alignSelf: 'center' })}>
         왁타버스 팀
       </p>
-      <div css={groupCss}>
-        <button
-          onClick={() => {
-            setStart(start - 1)
-            setEnd(end - 1)
-          }}
-        >
-          {'<'}
-        </button>
-        {imageList.map((image, inx) => {
-          return (
-            <div key={`image-${inx}`} css={imageCss}>
-              <Image src={image.src} alt={image.alt} height={512} />
-              <div css={artistNameCss}>
-                <p className="ko">{image.ko}</p>
-                <p className="en">{image.en}</p>
+
+      <div css={contentsCss}>
+        <div css={css({ height: '80px' })}>
+          {/* <button css={css({ height: '100%' })} onClick={goPrev}>
+            {'<'}
+          </button> */}
+        </div>
+        <div css={groupCss} ref={ref}>
+          {imageList.map((image, inx) => {
+            return (
+              <div key={`image-${inx}`} css={imageCss}>
+                <div css={artistImage}>
+                  <Image src={image.src} alt={image.alt} width={440} />
+                </div>
+                <div css={artistNameCss} className="artist-name">
+                  <p className="ko">{image.ko}</p>
+                  <p className="en">{image.en}</p>
+                </div>
               </div>
-            </div>
-          )
-        })}
-        ;
-        <button
-          onClick={() => {
-            setStart(start + 1)
-            setEnd(end + 1)
-          }}
-        >
-          {'>'}
-        </button>
+            )
+          })}
+        </div>
+        <div css={css({ height: '80px' })}>
+          {/* <button css={css({ height: '100%' })} onClick={goNext}>
+            {'>'}
+          </button> */}
+        </div>
       </div>
     </div>
   )
@@ -60,25 +85,34 @@ const groupWrapCss = css({
   flexDirection: 'column',
   justifyContent: 'center',
   padding: '100px',
+  gap: '30px',
 })
 
 const groupCss = css({
   display: 'flex',
   flexDirection: 'row',
-  justifyContent: 'center',
-  padding: '50px',
+  alignItems: 'center',
+
+  overflow: 'hidden',
   width: '1400px',
   height: '575px',
-  gap: '30px',
+  gap: `${gapWidth}px`,
 })
 
 const imageCss = css({
-  // width: '512px',
-  padding: '10px',
-  border: '1px solid white',
+  display: 'flex',
+  height: '100%',
+  flexDirection: 'column',
+  width: `${imageWidth}px`,
   '&:hover': {
     cursor: 'pointer',
   },
+})
+
+const artistImage = css({
+  flex: '1 1 auto',
+  display: 'flex',
+  alignItems: 'end',
 })
 
 const artistNameCss = css({
@@ -99,4 +133,9 @@ const artistNameCss = css({
   '.selected': {
     color: '#FFFFFF',
   },
+})
+
+const contentsCss = css({
+  display: 'flex',
+  flexDirection: 'row',
 })
