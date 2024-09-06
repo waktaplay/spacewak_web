3b2f2893b3c8701ef8f3cd39a0d3b654ba1f548f
ehdtn0321 <ehdtn0321@naver.com>
2024-09-06 17:57:44 +0900

    :hammer: Change: change color types
    색상 코드값 공통화 수정
    - black & white 추가
    - primary 수정
    - 주석 추가

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,30 +1,31 @@
+// black & white
+export const black = '#000000'
+export const white = '#FFFFFF'
+
 // grayscale
 export const grayScale = {
-  900: "#191A1A",
-  800: "#252627",
-  700: "#303133",
-  600: "#4E5053",
-  500: "#717479",
-  400: "#9DA1A8",
-  300: "#B4BBC2",
-  200: "#D3D5DB",
-  100: "#E5E7EC",
-  50: "#FBFBFB",
-};
+  900: '#191A1A',
+  800: '#252627',
+  700: '#303133',
+  600: '#4E5053',
+  500: '#717479',
+  400: '#9DA1A8',
+  300: '#B4BBC2',
+  200: '#D3D5DB',
+  100: '#E5E7EC',
+  50: '#FBFBFB',
+}
 
 // primary color
-export const primary = {
-  600: "#83FFA0",
-  700: "#95FFAD",
-};
+export const primary = '#47F998'
 
 export const secondary = {
-  greenLignt: "#87EF63",
-  greendark: "#4F6647",
-  pinkLight: "#F07BC1",
-  pinkDark: "#7B4F69",
-  blueLight: "#5FB3F0",
-  blueDark: "#536A7A",
-  orangeLight: "#F5A66E",
-  orangeDark: "#7D6350",
-};
+  greenLignt: '#87EF63', // 우왁굳
+  greendark: '#4F6647', // 우왁굳 bg
+  pinkLight: '#F07BC1', // 이세계아이돌
+  pinkDark: '#7B4F69', // 이세계아이돌 bg
+  blueLight: '#5FB3F0', // 고정멤버
+  blueDark: '#536A7A', // 고정멤버 bg
+  orangeLight: '#F5A66E', // 아카데미
+  orangeDark: '#7D6350', // 아카데미 bg
+}
