5ca3eddefcf0d72f783365dd8015a407c437e233
ehdtn0321 <61855652+ehdtn0321@users.noreply.github.com>
2024-09-19 21:50:51 +0900

    ✨ feat: 공통으로 사용 할 color types 추가 (#19)

---
+++ src/styles/colors.ts
@@ -0,0 +1,31 @@
+// black & white
+export const black = '#000000'
+export const white = '#FFFFFF'
+
+// grayscale
+export const grayScale = {
+  900: '#191A1A',
+  800: '#252627',
+  700: '#303133',
+  600: '#4E5053',
+  500: '#717479',
+  400: '#9DA1A8',
+  300: '#B4BBC2',
+  200: '#D3D5DB',
+  100: '#E5E7EC',
+  50: '#FBFBFB',
+}
+
+// primary color
+export const primary = '#47F998'
+
+export const secondary = {
+  greenLignt: '#87EF63', // 우왁굳
+  greendark: '#4F6647', // 우왁굳 bg
+  pinkLight: '#F07BC1', // 이세계아이돌
+  pinkDark: '#7B4F69', // 이세계아이돌 bg
+  blueLight: '#5FB3F0', // 고정멤버
+  blueDark: '#536A7A', // 고정멤버 bg
+  orangeLight: '#F5A66E', // 아카데미
+  orangeDark: '#7D6350', // 아카데미 bg
+}
