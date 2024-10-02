3f79d2f8a337c07c7154faf29c6bba2482dc36eb
ehdtn0321 <ehdtn0321@naver.com>
2024-10-02 19:47:28 +0900

    :lipstick: feat: add shadow types

---
+++ src/styles/shadow.ts
@@ -0,0 +1,11 @@
+export const SHADOW = {
+  lagre: `
+    box-shadow: 0px 12px 90px 0px rgba(0, 0, 0, 0.30);
+    backdrop-filter: blur(75px);
+  `,
+  medium: `
+    box-shadow: 0px 12px 60px 0px rgba(0, 0, 0, 0.30);
+    backdrop-filter: blur(75px);
+  `,
+  small: `backdrop-filter: blur(4px);;`,
+};
